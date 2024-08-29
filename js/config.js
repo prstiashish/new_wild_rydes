d64b7af97ad5c21625889e9648670c7437b6dfe3
prstiashish <ashish.raymohapatra@prsti.tech>
2024-08-29 20:27:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ll731dc5nj.execute-api.ap-south-1.amazonaws.com/rideDev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
